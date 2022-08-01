ba0bc29f4518673b787ab1a3ddab6ad80f1a8605
akonst17 <akonst17@gmail.com>
2022-08-01 11:51:38 +0300

    обновления Nursing; bui;d 25

--- Accuplacer/Classes/Misc/GlobalDefinitions.swift
+++ Accuplacer/Classes/Misc/GlobalDefinitions.swift
@@ -17,6 +17,7 @@ final class GlobalDefinitions {
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
 
+    static let appStoreUrl = "https://apps.apple.com/app/accuplacer-study-exam-app/id1563741227"
     static let appleAppID = "1563741227"
 
     static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
