c61f84c76f16af89f444cd4c713bcc280e2009ad
akonst17 <akonst17@gmail.com>
2022-09-19 21:20:25 +0300

    интеграция sdk 2.0; новые урлы; build 28

--- Accuplacer/Classes/Misc/GlobalDefinitions.swift
+++ Accuplacer/Classes/Misc/GlobalDefinitions.swift
@@ -6,21 +6,24 @@
 //
 
 final class GlobalDefinitions {
-    static let domainUrl = "https://exams.quantresear.ch"
+    static let domainUrl = "https://exams.mastrapi.com"
     static let apiKey = "YVoNsPdVQNoKwqEH"
-
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "qeUWF5hYvR3Q2IN3"
 
     static let applicationTag = "EXAM_ACCUPLACER"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "qeUWF5hYvR3Q2IN3"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "ORli7Tk18aWUSnxn"
 
     static let appStoreUrl = "https://apps.apple.com/app/accuplacer-study-exam-app/id1563741227"
     static let appleAppID = "1563741227"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
